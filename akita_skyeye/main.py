9dfd07c6fae586d86fa95f7985e7af7ba7dae3b5
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-03-15 11:58:12 -0400

    Create main.py

---
+++ akita_skyeye/main.py
@@ -0,0 +1,26 @@
+from akita_skyeye import DroneInterface, ReticulumInterface, parse_command, process_telemetry, load_drone_config
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+drone_config = load_drone_config()
+drone_id = drone_config["drone_id"]
+
+drone_interface = DroneInterface()
+reticulum_interface = ReticulumInterface(drone_id)
+
+def command_handler(destination, packet):
+    command = parse_command(packet)
+    if command:
+        drone_interface.send_command(json.dumps(command))
+
+reticulum_interface.register_command_handler(command_handler)
+
+while True:
+    telemetry = drone_interface.get_telemetry()
+    process_telemetry(telemetry)
+    drone_interface.check_failsafe(telemetry)
+    reticulum_interface.publish_telemetry(telemetry)
+    rt.Network.update()
+    time.sleep(1)

a36a131bebd78fcd0e2168f1d0b7bddb9c51de1a
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-03-15 11:54:15 -0400

    Create config.py

---
+++ akita_skyeye/config.py
@@ -0,0 +1,9 @@
+import json
+
+def load_drone_config(filepath="config/drone_config.json"):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+def load_reticulum_config(filepath="config/reticulum_config.json"):
+    with open(filepath, "r") as f:
+        return json.load(f)
